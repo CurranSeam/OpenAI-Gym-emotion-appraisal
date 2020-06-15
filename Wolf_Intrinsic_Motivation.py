f06808752a8f5cb16931a816c910883091bf0a5a
Curran Seam <curranseam@gmail.com>
2020-06-14 22:36:02 -0700

    Update Wolf_Intrinsic_Motivation.py

--- Wolf_Intrinsic_Motivation.py
+++ Wolf_Intrinsic_Motivation.py
@@ -1,9 +1,7 @@
-import gym
 import numpy as np
 from WolfDifferentRewardsEnvironment import WolfDifferentRewardsEnv
 from WolfExplorationEnvironment import WolfExplorationEnv
 from WolfDifferentTimestepEnvironment import WolfDifferentTimestepEnv
-from WolfJoyDistressEnvironment import WolfJoyDistressEnv
 from WolfFixedGoalEnvironment import WolfFixedGoalEnv
 import random
 from IPython.display import clear_output
@@ -23,12 +21,6 @@ def print_frames(frames):
 """Training the agent"""
 
 # Hyperparameters
-'''
-alpha = 0.3
-gamma = 1.0001
-epsilon = 0.1 
-'''
-
 alpha = 0.1
 gamma = 0.6
 epsilon = 0.1
@@ -39,17 +31,18 @@ eps = 1e-20
 all_epochs = []
 all_penalties = []
 
-#env = WolfExplorationEnv() 
+# Environments
+env = WolfExplorationEnv() 
 #env = WolfDifferentRewardsEnv() 
-env = WolfDifferentTimestepEnv()
+#env = WolfDifferentTimestepEnv()
 #env = WolfFixedGoalEnv()
 
 q_table = np.zeros([env.observation_space.n, env.action_space.n])
 
 for i in range(1, 10001): #original value 100001
-    #state = env.exploreReset() # Prey in states(0, 7, 14). Intial(36, 37, 38)
+    state = env.exploreReset() # Prey in states(0, 7, 14). Intial(36, 37, 38)
     #state = env.rewardsReset() # Different rewards scenario
-    state = env.centerReset()   # Different Timestep scenario
+    #state = env.centerReset()   # Different Timestep scenario
     #state = env.fixedGoalReset() # Fixed goal scenario
 
     epochs, penalties, reward, = 0, 0, 0
@@ -83,8 +76,6 @@ for i in range(1, 10001): #original value 100001
 v = np.zeros(env.nS)  # initialize value-function
 joy = np.zeros(env.nS)
 for i in range(1, 11):
-        print(joy)
-        #print(v)
         # Value iteration
         prev_v = np.copy(v)
         for s in range(env.nS):
@@ -98,13 +89,9 @@ for i in range(1, 11):
         if (np.sum(np.fabs(prev_v - v)) <= eps):
             print ('Value-iteration converged at iteration# %d.' %(i+1))
             break
-#v = [np.log2(i) for i in v if i > 0]
-#joy = [np.log2(i) if i > 0 else i for i in joy if i > 0]
+#print(joy)
 v = np.log2(v, out=np.zeros_like(v), where=(v!=0))
 joy = np.log2(joy, out=np.zeros_like(joy), where=(joy > 0))
-print(len(joy))
-#joy = [0 if np.isnan(x) else x for x in joy]
-print(joy)
 
 #print(env.P)
 print("Training finished.\n")
@@ -112,9 +99,9 @@ print("Training finished.\n")
 ####################################################################
 
 "render wolf: set environment to illustration's state"
-#env.s = 37  # Exploration Scenario
+env.s = 37  # Exploration Scenario
 #env.s = env.rewardsReset()   # Different rewards scenario
-env.s = env.centerReset() # Different Timestep scenario
+#env.s = env.centerReset() # Different Timestep scenario
 #env.s = env.fixedGoalReset() # Fixed goal scenario
 
 """Evaluate agent's performance after Q-learning"""
@@ -123,9 +110,9 @@ total_epochs, total_penalties = 0, 0
 episodes = 5
 
 for i in range(episodes):
-    #state = env.exploreReset()  # for exploration scenario
+    state = env.exploreReset()  # for exploration scenario
     #state = env.rewardsReset() # for the different rewards scenario
-    state = env.centerReset()
+    #state = env.centerReset()
     #state = env.fixedGoalReset() # Fixed goal scenario
     
     epochs, penalties, reward = 0, 0, 0
@@ -145,7 +132,6 @@ for i in range(episodes):
             'state': state,
             'action': action,
             'reward': reward
-            #'reward': q_table[state, action]
         }
     )
     epochs += 1
